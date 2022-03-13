15ece4aeb8774c40704f600b628db1bf385b5f60
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2022-03-13 12:13:49 -0700

    Move RuntimeError into its own translation unit.
    Instances of the RuntimeError exception need to be available for use not
    just in the Interpreter but in other components as well. Moving the
    RuntimeError class to its own translation unit helps make the latter
    possible.

---
+++ include/Interpreter/RuntimeError.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+
+#include "Scanner.h"
+
+namespace lox
+{
+/*!
+ * \class RuntimeError
+ * \brief The RuntimeError class represents a lox runtime error.
+ *
+ * We don't want lox throwing C++ exceptions or crashing when faced with
+ * erroneous runtime behavior. When the interpreter encounters an error
+ * it will throw a RuntimeError that is handled by Interpret() gracefully.
+ */
+class RuntimeError : public std::exception
+{
+public:
+    RuntimeError(const Token& token_, const std::string& message_) :
+        token(token_), message(message_) { }
+
+    Token       token;   /*!< Last Token processed before this error was thrown. */
+    std::string message; /*!< Message describing the runtime error. */
+}; // end RuntimeError
+} // end lox
