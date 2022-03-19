97a6986270086c629672539bf4348ef033d856e3
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2022-03-18 21:14:01 -0700

    Add a Return runtime exception and better document the RuntimeError header.
    This changeset includes documentation updates and a new runtime error
    type: Return. The Return exception is used to unwind the interpreter's
    stack after a User function call completes. Moreover, the exception
    carries with it the function's return value.

--- include/Interpreter/RuntimeError.h
+++ include/Interpreter/RuntimeError.h
@@ -1,7 +1,8 @@
 #pragma once
 
+#include <any>
 #include <string>
-#include <exception>
+#include <stdexcept>
 
 #include "Scanner.h"
 
@@ -16,13 +17,66 @@ namespace lox
  * it will throw a RuntimeError that is handled by Interpreter::Interpret()
  * gracefully.
  */
-class RuntimeError : public std::exception
+struct RuntimeError :
+    public std::runtime_error
 {
 public:
+    RuntimeError() = delete;
+
+    /*!
+     * \brief Construct a Lox runtime error with error token and message.
+     *
+     * \param token_ Last token seen at the moment the error was detected.
+     * \param message_ Error message to that will be reported to the User.
+     */
     RuntimeError(const Token& token_, const std::string& message_) :
-        token(token_), message(message_) { }
+        std::runtime_error(message_.c_str()),
+        token(token_),
+        message(message_)
+    { }
+
+    RuntimeError(const RuntimeError&) = default;
+    RuntimeError& operator=(const RuntimeError&) = default;
+
+    RuntimeError(RuntimeError&&) = default;
+    RuntimeError& operator=(RuntimeError&&) = default;
 
     Token       token;   /*!< Last Token processed before this error was thrown. */
     std::string message; /*!< Message describing the runtime error. */
 }; // end RuntimeError
+
+/*!
+ * \class Return
+ * \brief The Return class is used to return values from Lox functions.
+ *
+ * When a Lox function call completes a Return exception is thrown containing
+ * the return value of the function. Return provides a way for us unwind
+ * the interpreter's call stack while capturing the return value of the
+ * most recently executed User function.
+ */
+struct Return :
+    public std::runtime_error
+{
+public:
+    Return() = delete;
+
+    /*!
+     * \brief Constructs a Return exception with a return value.
+     *
+     * \param value_ The value being returned by the most recently executed
+     *               function.
+     */
+    Return (const std::any& value_) :
+        std::runtime_error(""),
+        value(value_)
+    { }
+
+    Return(const Return&) = default;
+    Return& operator=(const Return&) = default;
+
+    Return(Return&&) = default;
+    Return& operator=(Return&&) = default;
+
+    std::any value; /*!< Lox function return value. */
+}; // end Return
 } // end lox
