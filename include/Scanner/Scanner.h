ebdfb35bb416b008ea2e819469ba572675f400be
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2022-04-13 21:27:38 -0700

    Implement on demand source code scanning.
    The Scanner class implements the Lox lang scanner. Given a string of
    source text at construction, Scanner objects can return Token objects on
    demand via calls to the ScanToken() method.

---
+++ include/Scanner/Scanner.h
@@ -0,0 +1,246 @@
+#pragma once
+
+#include <cctype>
+#include <cstdint>
+#include <string>
+#include <unordered_map>
+
+namespace lox
+{
+/*!
+ * \class Token
+ * \brief The Token class wraps token data read by the Scanner.
+ */
+class Token
+{
+public:
+    /*!
+     * \enum TokenType
+     * \brief Lox token type.
+     */
+    enum class TokenType
+    {
+        /* Single-character tokens. */
+        kLeftParen,
+        kRightParen,
+        kLeftBrace,
+        kRightBrace,
+        kComma,
+        kDot,
+        kMinus,
+        kPlus,
+        kSemicolon,
+        kSlash,
+        kStar,
+
+        /* One or two character tokens. */
+        kBang,
+        kBangEqual,
+        kEqual,
+        kEqualEqual,
+        kGreater,
+        kGreaterEqual,
+        kLess,
+        kLessEqual,
+
+        /* Literals. */
+        kIdentifier,
+        kString,
+        kNumber,
+
+        /* Keywords. */
+        kAnd,
+        kClass,
+        kElse,
+        kFalse,
+        kFun,
+        kFor,
+        kIf,
+        kNil,
+        kOr,
+        kPrint,
+        kReturn,
+        kSuper,
+        kThis,
+        kTrue,
+        kVar,
+        kWhile,
+
+        kError,
+        kEof
+    }; // end TokenType
+
+    Token() = delete;
+
+    /*!
+     * \brief Construct a token.
+     *
+     * \param type Token type.
+     * \param lexeme Token lexeme.
+     * \param line Line number at which this token appears in the source code.
+     */
+    Token(TokenType type, const std::string& lexeme, int line);
+
+    ~Token() = default;
+
+    Token(const Token&) = default;
+    Token& operator=(const Token&) = default;
+
+    Token(Token&&) = default;
+    Token& operator=(Token&&) = default;
+
+    /*!
+     * \brief Return the type of this Token.
+     */
+    TokenType GetType() const { return type_; }
+
+    /*!
+     * \brief Return the type of this Token in string format.
+     */
+    std::string GetTypeStr() const { return kTokenToStr_.at(type_); }
+
+    /*!
+     * \brief Return the Token lexeme.
+     */
+    std::string GetLexeme() const { return lexeme_; }
+
+    /*!
+     * \brief Return the Token line number.
+     */
+    int GetLine() const { return line_; }
+
+private:
+    static const std::unordered_map<TokenType, std::string> kTokenToStr_; /*!< Map of TokenTypes to their corresponding string representation. */
+
+    TokenType   type_;    /*!< Token type. */
+    std::string lexeme_;  /*!< Token lexeme. */
+    int         line_;    /*!< Line number at which this token appears. */
+}; // end Token
+
+/*!
+ * \class Scanner
+ * \brief The Scanner class implements the Lox lang source code scanner.
+ *
+ * Scanner takes as input a Lox source string and emits Token objects on
+ * demand.
+ */
+class Scanner
+{
+public:
+    Scanner() = delete;
+
+    /*!
+     * \brief Construct a Scanner object with reference to some source code.
+     *
+     * \param source Lox lang source code.
+     */
+    explicit Scanner(const std::string& source);
+
+    ~Scanner() = default;
+
+    Scanner(const Scanner&) = delete;
+    Scanner& operator=(const Scanner&) = delete;
+    Scanner(Scanner&&) = delete;
+    Scanner& operator=(Scanner&&) = delete;
+
+    /*!
+     * \brief Return the next Token in the source code stream.
+     * \return The next Token in the source code stream. If the end of the
+     *         source code has been reached, a Token with TokenType EOF is
+     *         returned.
+     */
+    Token ScanToken();
+
+private:
+    static const std::unordered_map<std::string, Token::TokenType> kKeywords_; /*!< Map of keyword strings to their Token::TokenType. */
+
+    std::string SourceSubstring(int begin, int end) const
+        { return source_.substr(begin, end - begin); }
+
+    /*!
+     * \brief Return \c true if the entire source text has been scanned.
+     */
+    bool IsAtEnd() const
+        { return ('\0' == source_[current_]); }
+
+    /*!
+     * \brief Return \c true if \a c is a digit.
+     */
+    bool IsDigit(char c) const
+        { return ((c >= '0') && (c <='9')); }
+
+    /*!
+     * \brief Return \c true if \a c is alphabet character or underscore.
+     */
+    bool IsAlpha(char c) const
+        {
+            return (c >= 'a' && c <= 'z') ||
+                   (c >= 'A' && c <= 'Z') ||
+                   c == '_';
+        }
+
+    /*!
+     * \brief Return a Token with TokenType \a type.
+     */
+    Token MakeToken(Token::TokenType type) const
+        { return Token(type, SourceSubstring(start_, current_), line_); }
+
+    /*!
+     * \brief Return a Token whose lexeme contains the error message \a message.
+     */
+    Token ErrorToken(const std::string& message) const
+        { return Token(Token::TokenType::kError, message, line_); }
+
+    /*!
+     * \brief Advance the Scanner's source code cursor.
+     * \return The character currently under the Scanner's cursor prior to
+     *         updating the cursor.
+     */
+    char Advance()
+        { return source_[current_++]; }
+
+    /*!
+     * \brief Return the character currentl under the Scanner's cursor.
+     */
+    char Peek() const
+        { return source_.at(current_); }
+
+    /*!
+     * \brief Return the character one index past the current cursor position.
+     * \return If the the scanner is at the end of the source text, the null
+     *         character is returned.
+     */
+    char PeekNext() const
+        { return (IsAtEnd()) ? '\0' : source_[current_ + 1]; }
+
+    /*!
+     * \brief Return \c true if the char under the cursor matches \a expected.
+     */
+    bool Match(char expected);
+
+    /*!
+     * \brief Consume whitespace from the current cursor position.
+     */
+    void SkipWhitespace();
+
+    /*!
+     * \brief Return a string type Token.
+     */
+    Token String();
+
+    /*!
+     * \brief Return a number Token.
+     */
+    Token Number();
+
+    /*!
+     * \brief Return an identifier Token.
+     */
+    Token Identifier();
+
+    uint32_t    start_;   /*!< Source code start index. */
+    uint32_t    current_; /*!< Current source code index. */
+    uint32_t    line_;    /*!< Current line number. */
+    std::string source_;  /*!< Source code string. */
+}; // end Scanner
+} // end lox
---
+++ src/Scanner/Scanner.cc
@@ -0,0 +1,222 @@
+#include <cctype>
+#include <string>
+#include <unordered_map>
+
+#include "Scanner.h"
+
+namespace lox
+{
+const std::unordered_map<Token::TokenType, std::string>
+Token::kTokenToStr_ =
+{
+    {Token::TokenType::kLeftParen,    "LeftParen"},
+    {Token::TokenType::kRightParen,   "RightParen"},
+    {Token::TokenType::kLeftBrace,    "LeftBrace"},
+    {Token::TokenType::kRightBrace,   "RightBrace"},
+    {Token::TokenType::kComma,        "Comma"},
+    {Token::TokenType::kDot,          "Dot"},
+    {Token::TokenType::kMinus,        "Minus"},
+    {Token::TokenType::kPlus,         "Plus"},
+    {Token::TokenType::kSemicolon,    "Semicolon"},
+    {Token::TokenType::kSlash,        "Slash"},
+    {Token::TokenType::kStar,         "Star"},
+    {Token::TokenType::kBang,         "Bang"},
+    {Token::TokenType::kBangEqual,    "BangEqual"},
+    {Token::TokenType::kEqual,        "Equal"},
+    {Token::TokenType::kEqualEqual,   "EqualEqual"},
+    {Token::TokenType::kGreater,      "Greater"},
+    {Token::TokenType::kGreaterEqual, "GreaterEqual"},
+    {Token::TokenType::kLess,         "Less"},
+    {Token::TokenType::kLessEqual,    "LessEqual"},
+    {Token::TokenType::kIdentifier,   "Identifier"},
+    {Token::TokenType::kString,       "String"},
+    {Token::TokenType::kNumber,       "Number"},
+    {Token::TokenType::kAnd,          "And"},
+    {Token::TokenType::kClass,        "Class"},
+    {Token::TokenType::kElse,         "Else"},
+    {Token::TokenType::kFalse,        "False"},
+    {Token::TokenType::kFor,          "For"},
+    {Token::TokenType::kFun,          "Fun"},
+    {Token::TokenType::kIf,           "If"},
+    {Token::TokenType::kNil,          "Nil"},
+    {Token::TokenType::kOr,           "Or"},
+    {Token::TokenType::kPrint,        "Print"},
+    {Token::TokenType::kReturn,       "Return"},
+    {Token::TokenType::kSuper,        "Super"},
+    {Token::TokenType::kThis,         "This"},
+    {Token::TokenType::kTrue,         "True"},
+    {Token::TokenType::kVar,          "Var"},
+    {Token::TokenType::kWhile,        "While"},
+    {Token::TokenType::kError,        "ERROR"},
+    {Token::TokenType::kEof,          "EOF"}
+};
+
+Token::Token(TokenType type, const std::string& lexeme, int line) :
+    type_(type),
+    lexeme_(lexeme),
+    line_(line)
+{
+
+}
+
+const std::unordered_map<std::string, Token::TokenType>
+Scanner::kKeywords_ =
+{
+    {"and",    Token::TokenType::kAnd},
+    {"class",  Token::TokenType::kClass},
+    {"else",   Token::TokenType::kElse},
+    {"false",  Token::TokenType::kFalse},
+    {"for",    Token::TokenType::kFor},
+    {"fun",    Token::TokenType::kFun},
+    {"if",     Token::TokenType::kIf},
+    {"nil",    Token::TokenType::kNil},
+    {"or",     Token::TokenType::kOr},
+    {"print",  Token::TokenType::kPrint},
+    {"return", Token::TokenType::kReturn},
+    {"super",  Token::TokenType::kSuper},
+    {"this",   Token::TokenType::kThis},
+    {"true",   Token::TokenType::kTrue},
+    {"var",    Token::TokenType::kVar},
+    {"while",  Token::TokenType::kWhile}
+};
+
+bool Scanner::Match(char expected)
+{
+    if (IsAtEnd())
+        return false;
+
+    if (expected != source_[current_])
+        return false;
+
+    current_++;
+    return true;
+}
+
+void Scanner::SkipWhitespace()
+{
+    while (!IsAtEnd()) {
+        char c = Peek();
+        switch(c) {
+            case ' ':
+            case '\r':
+            case '\t':
+                Advance();
+                break;
+            case '\n':
+                line_++;
+                Advance();
+                break;
+            case '/':
+                if (PeekNext() == '/') {
+                    while (!IsAtEnd() && (Peek() != '\n'))
+                        Advance();
+                } else {
+                    return;
+                }
+                break;
+            default:
+                return;
+        }
+    }
+}
+
+Token Scanner::String()
+{
+    while (!IsAtEnd() && Peek() != '"') {
+        if (Peek() == '\n')
+            line_++;
+        Advance();
+    }
+
+    if (IsAtEnd())
+        return ErrorToken("Unterminated string.");
+
+    Advance();
+    return MakeToken(Token::TokenType::kString);
+}
+
+Token Scanner::Number()
+{
+    while (IsDigit(Peek()))
+        Advance();
+
+    /* Look for the fractional part. */
+    if (Peek() == '.' && IsDigit(PeekNext()))
+        /* Consume the '.'. */
+        Advance();
+
+    while (IsDigit(Peek()))
+        Advance();
+
+    return MakeToken(Token::TokenType::kNumber);
+}
+
+Token Scanner::Identifier()
+{
+    while (IsAlpha(Peek()) || IsDigit(Peek()))
+        Advance();
+
+    std::string candidate = SourceSubstring(start_, current_);
+    if (kKeywords_.find(candidate) != kKeywords_.end())
+        return MakeToken(kKeywords_.at(candidate));
+
+    return MakeToken(Token::TokenType::kIdentifier);
+}
+
+Scanner::Scanner(const std::string& source) :
+    start_(0),
+    current_(0),
+    line_(1),
+    source_(source)
+{
+
+}
+
+Token Scanner::ScanToken()
+{
+    SkipWhitespace();
+
+    start_ = current_;
+    if (IsAtEnd())
+        return MakeToken(Token::TokenType::kEof);
+
+    char c = Advance();
+
+    if (IsAlpha(c))
+        return Identifier();
+
+    if (IsDigit(c))
+        return Number();
+
+    using TokenType = Token::TokenType;
+    switch (c) {
+        case '(': return MakeToken(TokenType::kLeftParen);
+        case ')': return MakeToken(TokenType::kRightParen);
+        case '{': return MakeToken(TokenType::kLeftBrace);
+        case '}': return MakeToken(TokenType::kRightBrace);
+        case ';': return MakeToken(TokenType::kSemicolon);
+        case ',': return MakeToken(TokenType::kComma);
+        case '.': return MakeToken(TokenType::kDot);
+        case '-': return MakeToken(TokenType::kMinus);
+        case '+': return MakeToken(TokenType::kPlus);
+        case '/': return MakeToken(TokenType::kSlash);
+        case '*': return MakeToken(TokenType::kStar);
+        case '!':
+            return MakeToken(
+                Match('=') ? TokenType::kBangEqual: TokenType::kBang);
+        case '=':
+            return MakeToken(
+                Match('=') ? TokenType::kEqualEqual : TokenType::kEqual);
+        case '<':
+            return MakeToken(
+                Match('=') ? TokenType::kLessEqual : TokenType::kLess);
+        case '>':
+            return MakeToken(
+                Match('=') ? TokenType::kGreaterEqual : TokenType::kGreater);
+        case '"':
+            return String();
+    }
+    return ErrorToken("Unexpected character.");
+}
+
+} // end lox
